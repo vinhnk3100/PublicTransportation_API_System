d5fa8ee9b837cc4a4280d1130b3cc0b214e7483c
vinhnk3100 <vinhnk2300@gmail.com>
2023-02-07 01:44:29 +0700

    Feat[Change Rate limit]

--- src/middlewares/requestLimit.middleware.js
+++ src/middlewares/requestLimit.middleware.js
@@ -2,8 +2,10 @@ const rateLimit = require('express-rate-limit')
 
 exports.requestLimit = rateLimit({
     windowMs: 10 * 1000, // 10 seconds in milliseconds
-    max: 0,
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    max: 1,
+    message:
+		'Request too many, try again after 10 seconds',
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
