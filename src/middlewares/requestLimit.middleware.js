7cbb32b004865df8729f96a0a127a5953b6948b5
vinhnk3100 <vinhnk2300@gmail.com>
2023-02-07 09:16:26 +0700

    Feat[Limit] Cập nhật lên 15s

--- src/middlewares/requestLimit.middleware.js
+++ src/middlewares/requestLimit.middleware.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit')
 
 exports.requestLimit = rateLimit({
-    windowMs: 10 * 1000, // 10 seconds in milliseconds
+    windowMs: 15 * 1000, // 10 seconds in milliseconds
     max: 1,
     message:
 		'Request too many, try again after 10 seconds',
