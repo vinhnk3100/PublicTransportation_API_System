0e222ae8ccc8c2a1a241453c75ef36712fbe3e43
vinhnk3100 <vinhnk2300@gmail.com>
2023-02-07 09:24:15 +0700

    Feat[Rate limit] back to 10s

--- src/middlewares/requestLimit.middleware.js
+++ src/middlewares/requestLimit.middleware.js
@@ -1,12 +1,12 @@
 const rateLimit = require('express-rate-limit')
 
 exports.requestLimit = rateLimit({
-    windowMs: 15 * 1000, // 10 seconds in milliseconds
-    max: 1,
-    message:
-		'Request too many, try again after 10 seconds',
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 10 * 1000, // 10 seconds in milliseconds
+  max: 1,
+  message:
+  'Request too many, try again after 10 seconds',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
 module.exports = this
